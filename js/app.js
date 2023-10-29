8c398b4acccc132157da779b834f97c3a427ff64
Mahdi Razzaghi <103171417+MahdiRazzaghi808@users.noreply.github.com>
2023-10-29 11:45:47 +0330

    change font qustion-box

--- js/app.js
+++ js/app.js
@@ -8,19 +8,19 @@ for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
-        if (panel.style.maxHeight === "5rem") {
+        if (panel.style.maxHeight === "15rem") {
             panel.style.maxHeight = 0;
             this.querySelector("img").src = './asset/icon/arrow-next.svg';
 
         } else {
             allPanel.forEach(element => {
                 element.style.maxHeight = 0;
-
             });
             allArrow.forEach(element => {
                 element.src = './asset/icon/arrow-next.svg'
             })
-            panel.style.maxHeight = "5rem";
+            
+            panel.style.maxHeight = "15rem";
             this.querySelector("img").src = './asset/icon/arrow-down.svg';
 
         }
