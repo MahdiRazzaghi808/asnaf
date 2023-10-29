ee0f96a2a33af30b1deb5d27951e6c7a0b226642
Mahdi Razzaghi <103171417+MahdiRazzaghi808@users.noreply.github.com>
2023-10-29 11:16:49 +0330

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var acc = document.getElementsByClassName("accordion");
+var allPanel = document.querySelectorAll(".panel");
+var allArrow = document.querySelectorAll(".accordion img");
+var i;
+
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight === "5rem") {
+            panel.style.maxHeight = 0;
+            this.querySelector("img").src = './asset/icon/arrow-next.svg';
+
+        } else {
+            allPanel.forEach(element => {
+                element.style.maxHeight = 0;
+
+            });
+            allArrow.forEach(element => {
+                element.src = './asset/icon/arrow-next.svg'
+            })
+            panel.style.maxHeight = "5rem";
+            this.querySelector("img").src = './asset/icon/arrow-down.svg';
+
+        }
+    });
+}
